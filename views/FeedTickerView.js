0247728884efa62192954194f807a146ac172d65
Benjamin Goering <bengoering@gmail.com>
2013-02-03 09:09:41 -0800

    jslint and Make Ticker-related views properly render initially passed data (e.g. for caching

--- views/FeedTickerView.js
+++ views/FeedTickerView.js
@@ -10,10 +10,10 @@ var FeedTickerView = Backbone.View.extend(
         this.$el.hide();
         
         var outerHolder = $(document.createElement('div'));
-        outerHolder.addClass('hub-feed-item-holder')
+        outerHolder.addClass('hub-feed-item-holder');
         
         this.itemHolder = $(document.createElement('div'));
-        this.itemHolder.addClass('hub-feed-item-inner-holder')
+        this.itemHolder.addClass('hub-feed-item-inner-holder');
         this.$el.append(this.itemHolder);
 
         this._contentViewOpts = opts.contentViewOptions || {};
@@ -21,7 +21,7 @@ var FeedTickerView = Backbone.View.extend(
         
         this.$el.append(outerHolder.append(this.itemHolder));
         
-		var self = this;
+        var self = this;
         this.collection.on('add', this._insertItem, this);
     },
     className: "hub-FeedTickerView",
@@ -62,36 +62,36 @@ FeedTickerView.prototype._insertItem = function (item, col) {
         model: item,
         el: itemEl
     }, _getContentViewOpts(item)));
-	
-	this._animateAdd(itemEl, col, col.indexOf(item));
-	
+    
+    this._animateAdd(itemEl, col, col.indexOf(item));
+    
     return itemEl;
 };
 
 FeedTickerView.prototype._animateAdd = function(itemEl, col, index) {
-	var prev = col.at(index-1);
-	var next = col.at(index+1);
-	var self = this;
-	itemEl.hide();
-	
-	var origScroll = this.itemHolder.parent()[0].scrollHeight;
-    if (index == 0) {
-    	this.itemHolder.append(itemEl);
-    	itemEl.slideDown();
+    var prev = col.at(index-1);
+    var next = col.at(index+1);
+    var self = this;
+    itemEl.hide();
+    
+    var origScroll = this.itemHolder.parent()[0].scrollHeight;
+    if (index === 0) {
+        this.itemHolder.append(itemEl);
+        itemEl.slideDown();
     } else {
-    	prevEl = this.itemHolder.find('.hub-feed-item[data-hub-contentid="' + prev.get('id') + '"]');
-    	itemEl.insertBefore(prevEl).slideDown();
+        prevEl = this.itemHolder.find('.hub-feed-item[data-hub-contentid="' + prev.get('id') + '"]');
+        itemEl.insertBefore(prevEl).slideDown();
     }
     this.itemHolder.parent().addClass('nonEmpty');
     /*var newScroll = this.itemHolder.parent()[0].scrollHeight;
     var diff = newScroll - origScroll;
         
-	if (this.collection._initialized) {
-	    this.itemHolder.parent().scrollTop(diff);
-		this.itemHolder.parent().animate({
-			scrollTop: 0
-		}, 500);
-	}*/
+    if (this.collection._initialized) {
+        this.itemHolder.parent().scrollTop(diff);
+        this.itemHolder.parent().animate({
+            scrollTop: 0
+        }, 500);
+    }*/
 };
 
 return FeedTickerView;
