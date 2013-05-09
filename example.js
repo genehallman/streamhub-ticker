4ec82e1710774b0162ed1ca8295261bf04106cf8
Gene Hallman <genehallman@gmail.com>
2013-05-09 14:19:35 -0600

    Update example.js

--- example.js
+++ example.js
@@ -2,13 +2,27 @@ define(function(require) {
 	var Hub = require('streamhub-sdk');
 	var View = require('streamhub-ticker/views/TickerView');
 
-	return function(sdk, opts) {
-        var view = new View({
-            streams: Hub.Streams.forCollection($.extend({}, opts, {articleId: opts.articleId1}).start(),
-            feedStreams: Hub.Streams.forCollection($.extend({}, opts, {articleId: opts.articleId2}).start(),
-            el: document.getElementById(opts.elementId),
-        });
+	return function(el) {
+		var streams = Hub.StreamManager.create.livefyreStreams({
+		    network: "labs-t402.fyre.co",
+		    environment: "t402.livefyre.com",
+		    siteId: "303827",
+		    articleId: 'gene_publish_0'
+		});
+	
+		var feedStreams = Hub.StreamManager.create.livefyreStreams({
+		    network: "labs-t402.fyre.co",
+		    environment: "t402.livefyre.com",
+		    siteId: "303827",
+		    articleId: 'feed_ticker_0'
+		});
+
+
+	        var view = new View({el: el});
+		
+		streams.bind(view.main).start();
+		feedStreams.bind(view.feed).start();
          
-        return view;
-    };
+		return view;
+	};
 });
