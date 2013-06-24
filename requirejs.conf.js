2ba7248e00a06cedb7cfc917cece123cb0a58c5b
Gene Hallman <genehallman@gmail.com>
2013-06-24 12:53:45 -0600

    Update requirejs.conf.js

--- requirejs.conf.js
+++ requirejs.conf.js
@@ -1,14 +1,13 @@
 requirejs.config({
   paths: {
     jquery: 'lib/jquery/jquery',
-	jasmine: 'lib/jasmine/lib/jasmine-core/jasmine',
+    jasmine: 'lib/jasmine/lib/jasmine-core/jasmine',
     'jasmine-html': 'lib/jasmine/lib/jasmine-core/jasmine-html',
     'jasmine-jquery': 'lib/jasmine-jquery/lib/jasmine-jquery',
     text: 'lib/requirejs-text/text',
     hgn: 'lib/requirejs-hogan-plugin/hgn',
     hogan: 'lib/hogan/web/builds/2.0.0/hogan-2.0.0.amd',
-    base64: 'lib/base64/base64',
-    'streamhub-zepto': 'lib/streamhub-zepto/build/zepto.min'
+    base64: 'lib/base64/base64'
   },
   packages: [{
     name: 'streamhub-sdk',
